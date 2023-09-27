94d9a79573dd81b260b1eaed7b3b92ce13ca6fe8
rabee361 <rha60540@gmail.com>
2023-09-27 19:00:31 +0300

    changes

--- expenseapp/forms.py
+++ expenseapp/forms.py
@@ -2,10 +2,10 @@ from django.forms import ModelForm
 from django import forms
 from .models import *
 
-# class ItemForm(ModelForm):
-#     form = forms.BooleanField(widget=forms.HiddenInput, initial=True)
-#     class Meta:
-#         model = Item
-#         fields = ('item_name' , 'price',)
+class ItemForm(ModelForm):
+    form = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+    class Meta:
+        model = Item
+        fields = ('expense_name' , 'price', 'expense_type')
 
 
--- expenseapp/urls.py
+++ expenseapp/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     path('main/' , main ,name="main"),
     path('delete-item/<str:pk>', deletItem , name="delete-item"),
     path('add-item/' , addItem , name="add-item"),
-    path('chart/' , chart , name="chart"),
-
-
 ]
